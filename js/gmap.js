99cf7789991cef40f0b761267bc35f7c58725147
zita07 <35078852+zita07@users.noreply.github.com>
2021-05-28 19:45:17 +0100

    pink color and map size

--- js/gmap.js
+++ js/gmap.js
@@ -4,12 +4,20 @@ function initialize() {
     var mapProp = {
         center: myCenter,
         scrollwheel: false,
-        zoom:6,
+        zoom:12,
         zoomControl: false,
         mapTypeControl: false,
         streetViewControl: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        styles: [{ "featureType": "landscape", "stylers": [{ "color": "#f0ede5" }, { "visibility": "on" }] }, { "featureType": "poi", "stylers": [{ "saturation": -100 }, { "lightness": 51 }, { "visibility": "simplified" }] }, { "featureType": "road.highway", "stylers": [{ "color": "#f0ede5" }, { "visibility": "on" }] }, { "featureType": "road.arterial", "stylers": [{ "color": "#f0ede5" }, { "visibility": "on" }] }, { "featureType": "road.local", "stylers": [{ "color": "#14adf4" }, { "visibility": "off" }] }, { "featureType": "transit", "stylers": [{ "saturation": -100 }, { "visibility": "simplified" }] }, { "featureType": "administrative.province", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "labels", "stylers": [{ "visibility": "on" }, { "color": "#ed734e" }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#cbe6a3" }, { "visibility": "on" }] }]
+        // styles: [{ "featureType": "landscape", "stylers": [{ "color": "#f0ede5" }, { "visibility": "off" }] },
+        //  { "featureType": "poi", "stylers": [{ "saturation": -100 }, { "lightness": 51 }, { "visibility": "simplified" }] }, 
+        //  { "featureType": "road.highway", "stylers": [{ "color": "#f0ede5" }, { "visibility": "on" }] }, 
+        //  { "featureType": "road.arterial", "stylers": [{ "color": "#f0ede5" }, { "visibility": "on" }] }, 
+        //  { "featureType": "road.local", "stylers": [{ "color": "#14adf4" }, { "visibility": "off" }] }, 
+        //  { "featureType": "transit", "stylers": [{ "saturation": -100 }, { "visibility": "simplified" }] }, 
+        //  { "featureType": "administrative.province", "stylers": [{ "visibility": "off" }] }, 
+        //  { "featureType": "water", "elementType": "labels", "stylers": [{ "visibility": "on" }, { "color": "#ed734e" }] }, 
+        //  { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#cbe6a3" }, { "visibility": "on" }] }]
     };
 
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
